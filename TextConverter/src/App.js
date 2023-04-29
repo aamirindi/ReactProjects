5c3874fce1c446d8e0868e4b42c8a1db1b88cfe3
aamirindi <aamirindi@gmail.com>
2023-04-29 22:04:46 +0530

    updatedCamelCase

--- TextConverter/src/App.js
+++ TextConverter/src/App.js
@@ -1,12 +1,18 @@
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-
+import { useState } from "react";
 function App() {
+  const [text, setText] = useState("");
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
   return (
+    
     <>
       <Navbar />
       <div className="container" my-3>
-        <TextForm heading="Enter the Text to Analyze below "/>
+        <TextForm heading="Enter the Text to Analyze below" text={text} handleOnChange={handleOnChange}/>
       </div>
     </>
   );
--- TextConverter/src/components/TextForm.jsx
+++ TextConverter/src/components/TextForm.jsx
@@ -6,6 +6,7 @@ export default function TextForm(props) {
 
   const handleOnChange = (event) => {
     setText(event.target.value);
+    props.handleOnChange(event);
   };
 
   const handleUpClick = () => {
@@ -16,6 +17,20 @@ export default function TextForm(props) {
     let newText = text.toLowerCase();
     setText(newText);
   };
+  const handleCCClick = () => {
+    let newText = text.split(" ").map((word) => {
+      let char = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      return char
+    }).join(" ");  
+    setText(newText);
+  };
+
+  // split = 
+  // slice = 
+  // map =
+  // charAt =
+  // join = 
+
   const handleClearClick = () => {
     let newText = ('');
     setText(newText);
@@ -40,9 +55,13 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>
           Convert to Lowercase
         </button>
+        <button className="btn btn-primary mx-2" onClick={handleCCClick}>
+          Convert to CamelCase
+        </button>
         <button className="btn btn-primary mx-2" onClick={handleClearClick}>
           Clear
         </button>
+        
       </div>
       <div className="container">
         <h1>Your Text Summary</h1>
@@ -55,4 +74,5 @@ export default function TextForm(props) {
       </div>
     </div>
   );
-}
+  }
+  
